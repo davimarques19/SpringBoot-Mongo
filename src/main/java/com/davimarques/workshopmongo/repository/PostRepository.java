98f95b79d82dd7ae36ea121324e21dcf570c3312
Davi Marques <davimarques19@gmail.com>
2020-05-18 09:51:23 -0300

    Consulta simples com query methods

--- src/main/java/com/davimarques/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/davimarques/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.davimarques.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.davimarques.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
